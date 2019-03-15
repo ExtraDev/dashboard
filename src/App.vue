789830c8ccfd99391c23ee0aead1c7f565b3e500
CapitaineToinon <antoinesutter@gmail.com>
2019-03-15 18:55:21 +0100

    Fixed Vuetify + router, base project working now

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        to="/about"
+        flat
+      >
+        <span class="mr-2">About</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import Vue from 'vue'
+import Vue from "vue";
 import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
+import Vuetify from "vuetify";
 
-Vue.config.productionTip = false
+import "vuetify/dist/vuetify.min.css"; // Ensure you are using css-loader
+
+Vue.use(Vuetify);
+
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
