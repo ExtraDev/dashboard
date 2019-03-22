9dc07df183c930fc9b606c37246dbbb871abdec4
Jasmina Travnjak <animsai@gmail.com>
2019-03-22 16:28:07 +0100

    Added youtube feed

---
+++ src/services/YoutubeService.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+
+const base_url = "https://www.googleapis.com/youtube/v3";
+const token ="570833006615-q5i8adq5gedc5db0034fp9m03h0sd8an.apps.googleusercontent.com";
+
+export default {
+    getSubscriptionFeed() {
+        return axios.get(`${base_url}/subscriptions?access_token=${token}`,{
+            params: {
+                'mine': 'true',
+                'part': 'snippet,contentDetails'
+            } 
+        })
+    },
+    
+
+    
+}
