4008d4e2850d2cfa94bb2060d20fd33727d720db
Valentin Hutter <valentinhutter1296@gmail.com>
2019-03-23 13:23:59 +0100

    add how to use app

--- src/components/AboutTile.vue
+++ src/components/AboutTile.vue
@@ -2,11 +2,11 @@
   <v-card>
     <v-card-title>
       <h2>How to use</h2>
+      <p>Simply, click the "Plus" button on the right to add widgets (tiles) that you want.</p>
     </v-card-title>
   </v-card>
 </template>
 <script>
-import axios from "axios";
 export default {
   data() {
     return {};
