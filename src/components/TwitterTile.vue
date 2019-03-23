d1d09c0f3ad0148e469169ace427059542965e9f
Valentin Hutter <valentinhutter1296@gmail.com>
2019-03-23 13:10:34 +0100

    add twitter tile

---
+++ src/components/TwitterTile.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-layout row wrap>
+    <v-flex md12>
+      <v-card>
+        <v-card-title>
+          <h2>
+            <slot></slot>
+          </h2>
+        </v-card-title>
+
+        <v-flex xs12>
+          <Timeline id="RadioTeleSuisse" :sourceType="'likes'" :options="{ theme: 'dark', tweetLimit: '3' }" />
+        </v-flex>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+import { Tweet, Moment, Timeline } from 'vue-tweet-embed'
+
+export default {
+  components: {
+    Timeline
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.timeline-Widget {
+  border-radius: 0px;
+}
+</style>
